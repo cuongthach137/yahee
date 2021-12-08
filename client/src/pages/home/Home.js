2101830649447b95307c4881aa342171e7781943
cuong thach <cuongthach137@gmail.com>
2021-12-08 22:28:13 +0700

    redirect logged in to yahee

--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -2,9 +2,12 @@ import React, { useEffect } from "react";
 import CategoryArea from "../../components/CategoryArea/CategoryArea.component";
 import BestSellerArea from "../../components/BestSellerArea/BestSellerArea";
 import SeenProducts from "../../components/SeenProducts/SeenProducts";
+import { useHistory } from "react-router-dom";
 const Home = () => {
+  const history = useHistory();
   useEffect(() => {
     document.title = "C9STORE - HIGH LIKE ON C9";
+    history.push("/user/messenger");
   }, []);
 
   return (
