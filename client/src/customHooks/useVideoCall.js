dd1c1f77848269ad54f6fc983114ba353eb44a09
Cuong Thach <81078285+cuongthach137@users.noreply.github.com>
2022-05-05 20:02:42 +0700

    Update useVideoCall.js

--- client/src/customHooks/useVideoCall.js
+++ client/src/customHooks/useVideoCall.js
@@ -24,12 +24,24 @@ const useVideoCall = (myVideoRef, remoteVideoRef) => {
     // STUN server.
     const iceConfig = {
       iceServers: [
-        {
-          urls: "stun:stun.l.google.com:19302",
-        },
-        {
-          urls: "stun:stun1.l.google.com:19302",
-        },
+          {
+      urls: "stun:openrelay.metered.ca:80",
+    },
+    {
+      urls: "turn:openrelay.metered.ca:80",
+      username: "openrelayproject",
+      credential: "openrelayproject",
+    },
+    {
+      urls: "turn:openrelay.metered.ca:443",
+      username: "openrelayproject",
+      credential: "openrelayproject",
+    },
+    {
+      urls: "turn:openrelay.metered.ca:443?transport=tcp",
+      username: "openrelayproject",
+      credential: "openrelayproject",
+    },
       ],
     };
     const peer = new RTCPeerConnection(iceConfig);
