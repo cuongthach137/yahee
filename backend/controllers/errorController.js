184898901ccdc16724203a7c8dab601f2e175f03
Cuong Thach <81078285+cuongthach137@users.noreply.github.com>
2022-12-18 00:26:43 +0700

    Update errorController.js

--- backend/controllers/errorController.js
+++ backend/controllers/errorController.js
@@ -1,4 +1,4 @@
-const AppError = require("../utils/AppError");
+const AppError = require("../utils/appError");
 
 const handleCastErrorDB = (err) => {
   const message = `Invalid ${err.path}: ${err.value}.`;
